ea83ed36100ccff5ded691104ca318617c98fc13
Adam Rak <adam.rak@streamnovation.com>
2011-10-22 20:55:54 +0200

    implementing test set

--- tests/05_atomics/test.cpp
+++ tests/05_atomics/test.cpp
@@ -33,54 +33,11 @@
  *    
  */
 
-#ifndef CS_IMAGE_H
-#define CS_IMAGE_H
+#include <r800_state.h>
+#include <cs_image.h>
 
-#include <cstdint>
-#include <vector>
-#include <string>
-
-struct cs_image_header
+void do_test(r800_state& state)
 {
-  uint32_t magic;
-  uint32_t chksum;
-  uint32_t size; //size of the binary image, excluding the header
-  uint32_t lds_alloc;
-  uint32_t num_gprs;
-  uint32_t temp_gprs;
-  uint32_t global_gprs;
-  uint32_t stack_size; //stack size in enties
-  uint32_t thread_num; //how much wavefronts are allowed per SIMD
-  uint32_t dyn_gpr_limit;
-  uint32_t reserved[6];
-} __attribute__((packed));
+  compute_shader sh(&state, "shader.bin");
 
-struct r800_state;
-struct radeon_bo;
-
-struct compute_shader
-{
-  compute_shader(r800_state* state, const std::vector<char>& binary);
-  compute_shader(r800_state* state, std::string fname); //open raw for now
-
-  ~compute_shader();
-  
-  struct radeon_bo* binary_code_bo;
-  
-  int alloc_size; //allocated bo size in bytes
-  int lds_alloc; //local memory per workgroup in 32bit words
-  int num_gprs; //number or GPRs used by the shader
-  int temp_gprs; //number of temporary GRPs
-  int global_gprs; //number of global GPRs (on a SIMD)
-  int stack_size; //size of the stack to allocate
-  int thread_num; //per SIMD
-  int dyn_gpr_limit; //WTF?
-};
-
-
-// compute_shader cs_read_from_file(std::string fname);
-
-#endif
-
-
-
+}
