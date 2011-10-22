a7c96be01095cc103344677e1b831bcbe1df6bcc
Adam Rak <adam.rak@streamnovation.com>
2011-10-22 22:43:05 +0200

    test 05 works

--- tests/05_atomics/test.cpp
+++ tests/05_atomics/test.cpp
@@ -33,11 +33,100 @@
  *    
  */
 
+
 #include <r800_state.h>
 #include <cs_image.h>
+#include <evergreen_reg.h>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
 
 void do_test(r800_state& state)
 {
   compute_shader sh(&state, "shader.bin");
+  state.set_kms_compute_mode(true);
+  radeon_bo* write_buffer = state.bo_open(0, 1024*1024, 1024, RADEON_GEM_DOMAIN_VRAM, 0);
+  radeon_bo_map(write_buffer, 1);
+  
+  uint32_t *ptr = (uint32_t*)write_buffer->ptr;
 
+  for (int i = 0; i < 256; i++)
+  {
+    ptr[i] = 1000-i; ///< Fill it with a default value
+  } 
+  
+  radeon_bo_unmap(write_buffer);
+
+  radeon_bo* read_buffer = state.bo_open(0, 1024*1024, 1024, RADEON_GEM_DOMAIN_VRAM, 0);
+  radeon_bo_map(read_buffer, 1);
+  
+  ptr = (uint32_t*)read_buffer->ptr;
+
+  for (int i = 0; i < 256; i++)
+  {
+    ptr[i] = 42+i; ///< Fill it with the input pattern
+  } 
+  
+  radeon_bo_unmap(read_buffer);
+
+  vtx_resource_t vtxr;
+
+  memset(&vtxr, 0, sizeof(vtxr));
+
+  vtxr.id = SQ_FETCH_RESOURCE_cs;
+  vtxr.stride_in_dw = 1;
+  vtxr.size_in_dw = 256;
+  vtxr.vb_offset = 0;
+  vtxr.bo = read_buffer;
+  vtxr.dst_sel_x       = SQ_SEL_X;
+  vtxr.dst_sel_y       = SQ_SEL_Y;
+  vtxr.dst_sel_z       = SQ_SEL_Z;
+  vtxr.dst_sel_w       = SQ_SEL_W;
+  vtxr.endian = SQ_ENDIAN_NONE;
+  vtxr.num_format_all = SQ_NUM_FORMAT_INT;
+  vtxr.format = FMT_32;
+
+  state.set_vtx_resource(&vtxr, RADEON_GEM_DOMAIN_VRAM); ///< for vertex read
+  state.set_rat(11, write_buffer, 0, 1024*1024); ///< For RAT write
+  
+  state.set_gds(0, 0);
+  state.set_tmp_ring(NULL, 0, 0);
+  state.set_lds(0, 0, 0);
+  state.load_shader(&sh);
+  state.direct_dispatch({1}, {256});
+
+  cerr << "start kernel" << endl;
+  state.flush_cs();
+  
+  radeon_bo_wait(write_buffer);
+  
+  radeon_bo_map(write_buffer, 0);
+  
+  ptr = (uint32_t*)write_buffer->ptr;
+
+  cout << "result: " << endl;
+  
+  for (int i = 0; i < 256; i++)
+  {
+    cout << ptr[i] << " ";
+  }
+  
+  cout << endl;
+
+  for (int i = 0; i < 256; i++)
+  {
+    if (ptr[i] != 1042)
+    {
+      cerr << i << " : " << ptr[i] << " != " << 1042 << " (does not equal  the expected value)" << endl; 
+      throw runtime_error("Error: shader result verify failed");
+    }
+  }
+    
+  radeon_bo_unmap(write_buffer);  
+  
+  radeon_bo_unref(write_buffer);
+  radeon_bo_unref(read_buffer);
+  
+  cerr << "OK" << endl;
 }
